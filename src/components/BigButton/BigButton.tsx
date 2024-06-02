f1d481420a2250d6707b2f47961beac338f4fb52
Даниил Гришаев <raipot228@gmail.com>
2024-06-02 22:57:16 +0600

    almost complete SendWorkModal

--- src/components/BigButton/BigButton.tsx
+++ src/components/BigButton/BigButton.tsx
@@ -1,9 +1,9 @@
 import classes from "./BigButton.module.scss";
 
-const BigButton = (props: { children: React.ReactNode, onClick: () => void, className?: string }) => {
+const BigButton = (props: { children: React.ReactNode, onClick: () => void, buttonClassName?: string, spanClassName?: string }) => {
     return (
-        <button className={`${props.className} ${classes.button}`} onClick={props.onClick}>
-            <span className={classes.text}>{props.children}</span>
+        <button className={`${props.buttonClassName} ${classes.button}`} onClick={props.onClick}>
+            <span className={`${props.spanClassName} ${classes.text}`} >{props.children}</span>
         </button>
     )
 }
--- src/components/Modals/SendWork/SendWorkModal.tsx
+++ src/components/Modals/SendWork/SendWorkModal.tsx
@@ -1,5 +1,7 @@
 import BigButton from '../../BigButton/BigButton';
 import classes from './SendWorkModal.module.scss';
+import { MdOutlineUpload } from "react-icons/md";
+import { useForm } from 'react-hook-form';
 
 type SendWorkModalProps = {
   active: boolean;
@@ -7,17 +9,54 @@ type SendWorkModalProps = {
   onSubmit: () => void;
 };
 
-const SendWorkModal = ({ active, onClose, onSubmit}: SendWorkModalProps) => {
+const SendWorkModal = ({ active, onClose}: SendWorkModalProps) => {
+    const { 
+        register,
+        formState: { isValid },
+        handleSubmit,
+        reset,
+    } = useForm({mode: "onBlur"});
+
+    const onSubmit = (data: unknown) => {
+        alert(JSON.stringify(data))
+        reset();
+    }
+
     if (!active) {
         return null;
     }
+
     return (
         <div className={classes.wrapper} onClick={onClose}>
             <div className={classes.container} onClick={(event) => event.stopPropagation()}>
-                <div className={classes.footer}>
-                    <BigButton className={classes.button} onClick={onClose}>Отменить</BigButton>
-                    <BigButton onClick={onSubmit}>Загрузить</BigButton>
+                <div className={classes.header}>
+                    <MdOutlineUpload className={classes.image} />
+                    <h2 className={classes.header_title}>Загрузить работу</h2>
                 </div>
+
+                <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+                    <label>
+                        Название задания
+                        <input {...register('exercise', { required: true })} placeholder='ООАиП'/>
+                    </label>
+                    <label>
+                        Дисциплина
+                        <select {...register("subject", { required: true })}>
+                            <option value="ooaip">ООАиП</option>
+                            <option value="practicum">Практикум</option>
+                        </select>
+                    </label>
+                    <label>
+                        Ссылка на задание
+                        <input type="url" placeholder="https://github.com/" {...register("link", {required: true})} />
+                    </label>
+                    <label>
+                        Комментарий к работе
+                        <textarea {...register("comment")} />
+                    </label>
+
+                    <input type='submit' disabled={!isValid}></input>
+                </form>
             </div>
         </div>
     )
